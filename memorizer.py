28b7164f6fa0d73b0d56f8d0e6a2aef68b490be8
pawangh <pawan24ghildiyal@gmail.com>
2026-01-16 18:08:39 +0000

    Add files via upload

--- memorizer.py
+++ memorizer.py
@@ -1,16 +1,31 @@
 import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
 screen = tkinter.Tk()
 screen.geometry("400x400")
 screen.title(" multiple factors ")
 
+def delete():
+    indexs = lb.curselection()
+    lb.delete(indexs)
+
+def save():
+    file1 = tkinter.filedialog.asksaveasfile()
+    if file1 != None:
+        for item in lb.get(0,tkinter.END):
+            print(item,file=file1)
+
 def add():
     edd=e1.get()
-    lb.insert(tkinter.END,edd)
-    e1.delete(0,tkinter.END)
+    if edd == "":
+        tkinter.messagebox.askyesno("this entry is empty","do you want it in your entry?")
+    else:
+        lb.insert(tkinter.END,edd)
+        e1.delete(0,tkinter.END)
 
 b1=tkinter.Button(screen,text="open")
-b2=tkinter.Button(screen,text="delete")
-b3=tkinter.Button(screen,text="save")
+b2=tkinter.Button(screen,text="delete",command=delete)
+b3=tkinter.Button(screen,text="save",command=save)
 b4=tkinter.Button(screen,text="add",command=add)
 e1=tkinter.Entry(screen)
 lb=tkinter.Listbox(screen)
